50623e6f4849abbbea7a3ad149526e2920cb8f85
amitk9627 <ramitgzp56@gmail.com>
2024-10-20 13:01:26 +0530

    body parser install now body limit is 50mb

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 require("dotenv").config();
 const userRoute = require("./routes/users/user.js");
+const bodyParser = require('body-parser')
 const BikePurchaseRoute = require("./routes/BikePurchase/bikePurchase.js");
 const app = express();
 const cors = require("cors");
 
 // Middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json({ limit: '50mb' }))
+
+
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/bike", BikePurchaseRoute);

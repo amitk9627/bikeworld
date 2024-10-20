7f2ccb2935ea8e719879199cb2961b9acaaeec22
amitk9627 <ramitgzp56@gmail.com>
2024-10-20 12:45:40 +0530

    null check backend and default null

--- model/BikePurchase/bikePurchase.js
+++ model/BikePurchase/bikePurchase.js
@@ -4,73 +4,91 @@ const BikePurchaseSchema = new mongoose.Schema(
   {
     personName: {
       type: String,
-      trim: true
+      trim: true,
+      default: null,
     },
     personPhoto: {
       type: String,
+      default: null,
     },
     mobileNo: {
       type: String,
-
+      default: null,
       trim: true,
     },
     gender: {
       type: String,
+      default: null,
     },
     panNo: {
       type: String,
+      default: null,
     },
     panUpload: {
       type: String,
+      default: null,
     },
     aadharNo: {
       type: String,
+      default: null,
     },
     aadharFront: {
       type: String,
+      default: null,
     },
     aadharBack: {
       type: String,
+      default: null,
     },
     engineNo: {
       type: String,
+      default: null,
     },
     chasisNo: {
       type: String,
+      default: null,
     },
     emailID: {
       type: String,
       trim: true,
+      default: null,
     },
     address: {
       type: String,
-
+      default: null,
       trim: true,
     },
     cityName: {
       type: String,
       trim: true,
+      default: null,
     },
     pinCode: {
       type: String,
       trim: true,
+      default: null,
     },
     registrationNo: {
       type: String,
       trim: true,
+      default: null,
     },
     purchaseDate: {
       type: Date,
+      default: null,
     },
     bikeNo: {
       type: String,
+      default: null,
     },
     bikeUpload: {
       type: String,
+      default: null,
     },
     remarks: {
       type: String,
       trim: true,
+      default: null,
     },
   },
   {
